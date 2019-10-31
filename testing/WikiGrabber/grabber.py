e9920decfad7e134cc6d22863ac14560f6a48c96
Tg Tiger <tgakatiger@gmail.com>
2019-10-31 13:07:46 +0300

    Testing from Fedora 31 Workstation

--- testing/WikiGrabber/grabber.py
+++ testing/WikiGrabber/grabber.py
@@ -1,33 +1,38 @@
 import re
-import requests
 from time import time
+import requests
+
 
 class Grabber():
-    def __init__(self, *args, **kwargs):
+    '''Grab page from Wikipedia.org'''
+    def __init__(self, **kwargs):
         self.pattern = r'<a\s.*?href="(.+?)".*?>(.+?)</a>'
         # self.pattern = r'<a\s.*?href="(.+?)".*</a>'
         self.url = kwargs['url']
 
     def get_regexp(self, pattern=None):
+        '''Return regex object'''
         pattern = pattern or self.pattern
         regexp = re.compile(pattern)
         return regexp
 
     def grab(self):
+        '''Grab text from regex object'''
         if self.url is None:
-            return
+            return None
         return self.get_regexp().findall(requests.get(self.url).text)
 
 
 def main():
+    '''Main function'''
     grabber = Grabber(url='https://en.wikipedia.org/wiki/Main_Page')
     grabs = grabber.grab()
     print(f'Len: {len(grabs)}')
-    # create_file('\n'.join('\t'.join((i[0], i[1])) for i in grabs))
-    
+    create_file('\n'.join('\t'.join((i[0], i[1])) for i in grabs))
 
 
 def create_file(data, filename=None):
+    '''Create text file from data'''
     filename = filename or 'file-{}.txt'.format(time())
     with open(filename, 'w') as file:
         file.write(data)
@@ -35,4 +40,3 @@ def create_file(data, filename=None):
 
 if __name__ == "__main__":
     main()
-
