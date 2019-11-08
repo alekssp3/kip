227176c7ea3ca055fb3d4cc64fa2fc1994f2ccd6
Tg Tiger <tgakatiger@gmail.com>
2019-11-08 17:24:45 +0300

    Using requests_html

--- testing/WikiGrabber/asyncio_clean.py
+++ testing/WikiGrabber/asyncio_clean.py
@@ -1,9 +1,25 @@
-import requests
-import asyncio
+# import requests
+from requests_html import AsyncHTMLSession
+# import asyncio
 from time import time
+import os
+import sys
+# sys.path.insert(0, os.path.abspath(os.getcwd()))
+# from grabber2 import create_file_from_list
 
 
-file = 'file-1573211536.3653588.txt'
+#file = 'file-1573211536.3653588.txt'
+
+file = 'width1.txt'
+
+RESULTS = []
+
+
+def create_file_from_list(data, filename=None):
+    '''Create text file from data'''
+    filename = filename or 'file-{}.txt'.format(time())
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write('\n'.join(i for i in data))
 
 
 def load(filename=None):
@@ -12,19 +28,38 @@ def load(filename=None):
             yield from file.readlines()
 
 
-async def ping(url):
+def ping_creator(session, url):
+    async def inner():
+        try:
+            print(f'Work with {url}')
+            r = await session.get(url)
+            links = r.html.absolute_links
+            RESULTS.extend(links)
+            # print(len(links))
+        except:
+            pass
+    return inner
+
+
+async def ping(session, url):
     # print(f'Work with {url}')
-    return requests.get(url)
+    r = await session.get(url)
+    return r
 
 
 def main():
-    loop = asyncio.get_event_loop()
-    tasks = [ping(i) for i in load(file)]
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
+    # loop = asyncio.get_event_loop()
+    # tasks = [ping(i) for i in load(file)]
+    # loop.run_until_complete(asyncio.wait(tasks))
+    # loop.close()
+    session = AsyncHTMLSession()
+    tasks = [ping_creator(session, url) for url in load(file)]
+    session.run(*tasks)
 
 
 if __name__ == "__main__":
     start = time()
     main()
     print(time() - start)
+    print(len(RESULTS))
+    create_file_from_list(RESULTS)
